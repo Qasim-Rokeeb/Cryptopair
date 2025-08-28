2401466285b67cbaa2f4f121d457acbbefd805a5
Rokeeb Qasim <qasimrokeeb@gmail.com>
2025-08-28 18:59:31 +0000

    swap timing-function to ease-out

--- src/components/game/GameCard.tsx
+++ src/components/game/GameCard.tsx
@@ -70,14 +70,14 @@ export function GameCard({ card, isFlipped, isMatched, isMismatched, onClick, is
       style={{ animationDelay: `${index * 40}ms`, ...matchedStyle }}>
       <div
         className={cn(
-          'relative w-full h-full min-h-[12rem] rounded-lg preserve-3d transition-transform duration-500 ease-in-out perspective',
+          'relative w-full h-full min-h-[12rem] rounded-lg preserve-3d transition-transform duration-500 ease-out perspective',
           (isFlipped || isMatched) && 'rotate-y-180',
           !isMatched && 'cursor-pointer'
         )}
       >
         {/* Front of card */}
         <div className="absolute w-full h-full backface-hidden rounded-lg bg-secondary/50 dark:bg-white/5 dark:backdrop-blur-lg border-2 border-primary/20 flex items-center justify-center p-4 hover:border-primary transition-colors">
-          <Icon name={card.icon} className="w-16 h-16 text-primary dark:opacity-100 transition-transform duration-300 ease-in-out group-hover:scale-110" />
+          <Icon name={card.icon} className="w-16 h-16 text-primary dark:opacity-100 transition-transform duration-300 ease-out group-hover:scale-110" />
         </div>
         {/* Back of card */}
         <div
